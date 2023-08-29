2e80a697ffe91a049ae5a5572a5ca52166865c7d
TorTor74 <55371769+TorTor74@users.noreply.github.com>
2023-08-30 00:24:34 +0500

    add element coze zaputalas

---
+++ src/lib/DayCalendar.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { getContext } from "svelte";
+	import DayOfCalendar from "./DayOfCalendar.svelte";
+	import DayOfYear from "./DayOfYear.svelte";
+	import { getMonth } from "date-fns";
+	import DayOfYearNew from "./DayOfYearNew.svelte";
+	const today = getContext("today");
+	const month = getMonth(today);
+</script>
+
+<div class="container">
+	<DayOfCalendar />
+	<div class="month">
+		<DayOfYearNew />
+		<div class="even">Нет событий</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		width: 100%;
+		.month {
+			background: #e5e6e7;
+			:global(.dateMonth){
+				margin: 6px  13px 0 11px;
+			}
+			.even {
+				display: flex;
+				width: 321px;
+				height: 446px;
+				padding: 178px 68px 249px 68px;
+				flex-direction: column;
+				align-items: center;
+				flex-shrink: 0;
+				color: #A5A6A7;
+text-align: right;
+font-family: Helvetica ;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+			}
+		}
+	}
+</style>
---
+++ src/lib/DayOfCalendar.svelte
@@ -0,0 +1,78 @@
+<script>
+	import { format } from "date-fns";
+	import DayOfWeek from "./DayOfWeek.svelte";
+	import TimeLine from "./TimeLine.svelte";
+	import Line from "./Line.svelte";
+    import { getContext } from "svelte";
+	const today = new Date();
+	const day=getContext('today')
+	// const day = new Date();
+</script>
+
+<div class="day">
+	<div class="header">
+		<div class="dateDay">
+			<b>{format($day, "d MMMM")}</b>
+			<span>{format($day, "yyyy")}<b>г.</b></span>
+		</div>
+		<div class="dateWeek">
+			{format($day, "EEEE")}
+		</div>
+	</div>
+	<div class="time">
+		<TimeLine />
+		<DayOfWeek day={$day} {today} />
+		<Line />
+	</div>
+</div>
+
+<style lang="scss">
+	.day {
+		width: 869px;
+		flex-shrink: 0;
+		padding: 4px 0 0 16px;
+	}
+	.header {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: start;
+		border-bottom: 1px solid #e5e5e5;
+
+		.dateDay {
+			color: #000;
+			text-align: right;
+			font-family: Helvetica;
+			font-size: 26px;
+			font-style: normal;
+			line-height: normal;
+		}
+		.dateWeek {
+			color: #000;
+			text-align: right;
+			font-family: Helvetica;
+			font-size: 16px;
+			font-style: normal;
+			font-weight: 400;
+			line-height: normal;
+		}
+	}
+	.time {
+		display: flex;
+		width: 100%;
+		position: relative;
+		:global(.item) {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			flex-shrink: 0;
+			:global(.hours){
+				width: 764px;
+				border-top:3px solid #EDEDED;
+			}
+			:global(.allDay){
+				border: unset;
+			}
+		}
+	}
+</style>
